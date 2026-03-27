798473e50d0e012a34d8cbaf4dd92a6c251f3904
Graziani Nicola <graziani.nicola2000@gmail.com>
2026-03-27 11:45:06 +0100

    Added User and Loan entities with corresponding database tables and relationships.

--- src/main/java/nicolagraziani/entities/Loan.java
+++ src/main/java/nicolagraziani/entities/Loan.java
@@ -1,4 +1,55 @@
 package nicolagraziani.entities;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "loan")
 public class Loan {
+    @Id
+    @GeneratedValue
+    @Column(name = "loan_id")
+    private UUID loanId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "catalog_item_id")
+    private CatalogItem item;
+
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    @Column(name = "expected_return_date")
+    private LocalDate expectedReturnDate;
+
+    @Column(name = "effective_return_date")
+    private LocalDate effectiveReturnDate;
+
+    public Loan() {
+    }
+
+    public Loan(User user, CatalogItem item, LocalDate startDate, LocalDate expectedReturnDate, LocalDate effectiveReturnDate) {
+        this.user = user;
+        this.item = item;
+        this.startDate = startDate;
+        this.expectedReturnDate = expectedReturnDate;
+        this.effectiveReturnDate = effectiveReturnDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Loan{" +
+                "loanId=" + loanId +
+                ", user=" + user +
+                ", item=" + item +
+                ", startDate=" + startDate +
+                ", expectedReturnDate=" + expectedReturnDate +
+                ", effectiveReturnDate=" + effectiveReturnDate +
+                '}';
+    }
 }
--- src/main/java/nicolagraziani/entities/User.java
+++ src/main/java/nicolagraziani/entities/User.java
@@ -1,9 +1,48 @@
 package nicolagraziani.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue
+    @Column(name = "user_id")
+    private UUID userId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String surname;
+
+    @Column(name = "date_of_birth", nullable = false)
+    private LocalDate dateOfBirth;
+
+    @Column(name = "membership", unique = true)
+    private int membershipCode;
+
+    public User() {
+    }
+
+    public User(String name, String surname, LocalDate dateOfBirth, int membershipCode) {
+        this.name = name;
+        this.surname = surname;
+        this.dateOfBirth = dateOfBirth;
+        this.membershipCode = membershipCode;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", membershipCode=" + membershipCode +
+                '}';
+    }
 }
